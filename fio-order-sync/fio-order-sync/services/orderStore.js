bbe53c208d67434e059581a93e85fc65fce10bef
Fulle <fullerko@seznam.cz>
2026-04-27 18:14:50 +0200

    Fix Fio sync shop API URL

--- fio-order-sync/fio-order-sync/services/orderStore.js
+++ fio-order-sync/fio-order-sync/services/orderStore.js
@@ -1,4 +1,4 @@
-const SHOP_API = "http://127.0.0.1:5000";
+const SHOP_API = process.env.SHOP_API || "https://botyzahubicku.cz";
 
 export async function getOrderByVariableSymbol(variableSymbol) {
   return { id: variableSymbol };
